e79370b97eebf6f5cb6e82382688376c4db00db9
WMeyerIII <valodier@icloud.com>
2022-10-10 12:19:21 -0400

    Header changes background/hover border color if not at top of scroll-page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+      $(".header").addClass("active");
+      $(".flex-wrapper").addClass("active");
+    } else {
+      $(".header").removeClass("active");
+      $(".flex-wrapper").addClass("active");
+    }
+  });
+});
